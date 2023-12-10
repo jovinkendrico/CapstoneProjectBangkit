4bbb5400453c4a6192f17fa9ad1e5bf192f9a65a
jovinkendrico <jovinkendrico@gmail.com>
2023-12-10 18:54:59 +0700

    update pengecekan mobil sudah selesai

--- app/src/main/java/com/example/capstoneproject/HomeActivity.kt
+++ app/src/main/java/com/example/capstoneproject/HomeActivity.kt
@@ -1,11 +1,26 @@
 package com.example.capstoneproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capstoneproject.databinding.ActivityHomeBinding
+import com.example.capstoneproject.ui.auth.login.LoginActivity
+import com.example.capstoneproject.ui.auth.register.RegisterActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginActivityButton.setOnClickListener{
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+
+        binding.registerActivityButton.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
 }
